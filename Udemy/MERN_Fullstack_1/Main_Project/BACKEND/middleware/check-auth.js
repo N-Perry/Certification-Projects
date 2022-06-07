d7317be5c70547d621b2550aa7820d8321a18bc5
Nick Perry <nperry43@gmail.com>
2022-06-07 16:48:46 -0700

    custom middleware that uses jsonwebtoken to authorize requests

---
+++ Udemy/MERN_Fullstack_1/Main_Project/BACKEND/middleware/check-auth.js
@@ -0,0 +1,22 @@
+const jwt = require("jsonwebtoken");
+
+const HttpError = require("../models/http-error");
+
+module.exports = (req, res, next) => {
+  if (req.method === "OPTIONS") {
+    return next();
+  }
+
+  try {
+    const token = req.headers.authorization.split(" ")[1]; // Authorization: 'Bearer TOKEN'
+    if (!token) {
+      throw new Error("Authentication Failed!");
+    }
+    const decodedToken = jwt.verify(token, "supersecret_dont_share"); // returns an obj with info we bcrypted into token
+    req.userData = { userId: decodedToken.userId }; // you can always dynamically add data to the request object like this
+    next();
+  } catch (err) {
+    const error = new HttpError("Authentication failed!", 403);
+    return next(error);
+  }
+};
